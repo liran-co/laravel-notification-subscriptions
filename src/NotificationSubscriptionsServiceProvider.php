a3d11c890cad4f73c1794de02330a07a6df934b9
Liran Cohen <l@liran.co>
2020-05-20 11:34:31 -0400

    Move config default to register function.

--- src/NotificationSubscriptionsServiceProvider.php
+++ src/NotificationSubscriptionsServiceProvider.php
@@ -11,16 +11,16 @@ class NotificationSubscriptionsServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/notification-subscriptions.php' => config_path('notification-subscriptions.php'),
         ]);
-    
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/notification-subscriptions.php', 'notification-subscriptions'
-        );
         
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
     
     public function register()
     {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/notification-subscriptions.php', 'notification-subscriptions'
+        );
+        
     	$this->app->register(\LiranCo\NotificationSubscriptions\Providers\EventServiceProvider::class);
     }
 }
