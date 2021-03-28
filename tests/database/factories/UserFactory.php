5193727fd188d6fde7997570a7b6017d589074a9
Andrés Santibáñez <santibanez.andres@gmail.com>
2021-03-27 20:39:42 -0500

    Fixed formatting

--- tests/database/factories/UserFactory.php
+++ tests/database/factories/UserFactory.php
@@ -3,9 +3,9 @@
 
 namespace LiranCo\NotificationSubscriptions\Tests\database\factories;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use LiranCo\NotificationSubscriptions\Tests\TestModels\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory
 {
@@ -19,11 +19,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name'              => $this->faker->name,
+            'email'             => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => bcrypt('password'),
-            'remember_token' => Str::random(10),
+            'password'          => bcrypt('password'),
+            'remember_token'    => Str::random(10),
         ];
     }
 }
